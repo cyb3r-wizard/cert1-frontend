eae3478d13b44d5d10ec7e28540a86de6eb79b29
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2025-04-03 18:09:03 -0300

    feat: cirst fommit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const PORT = process.env.PORT ?? 3000
+
+const app = express()
+const users = [
+	{
+		username: 'admin',
+		name: 'Gustavo Alfredo Marín Sáez',
+		password: '1b6ce880ac388eb7fcb6bcaf95e20083:341dfbbe86013c940c8e898b437aa82fe575876f2946a2ad744a0c51501c7dfe6d7e5a31c58d2adc7a7dc4b87927594275ca235276accc9f628697a4c00b4e01'// certamen123
+	}
+]
+
+app.use(express.static('public'))
+// Escriba su código a partir de aquí
+
+// Hasta aquí
+
+app.listen(PORT, (error) => {
+	if (error) {
+		console.error(`No se puede ocupar el puerto ${PORT} :(`)
+		return
+	}
+
+	console.log(`Escuchando en el puerto ${PORT}`)
+})
