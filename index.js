485e6ed765d1b37f6abcde86403db506352ae734
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2025-04-03 20:13:48 -0300

    formateo de código + ruta hola mundo

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-import express from 'express'
+import express from "express";
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3000;
 
-const app = express()
+const app = express();
 const users = [
-	{
-		username: 'admin',
-		name: 'Gustavo Alfredo Marín Sáez',
-		password: '1b6ce880ac388eb7fcb6bcaf95e20083:341dfbbe86013c940c8e898b437aa82fe575876f2946a2ad744a0c51501c7dfe6d7e5a31c58d2adc7a7dc4b87927594275ca235276accc9f628697a4c00b4e01'// certamen123
-	}
-]
+  {
+    username: "admin",
+    name: "Gustavo Alfredo Marín Sáez",
+    password:
+      "1b6ce880ac388eb7fcb6bcaf95e20083:341dfbbe86013c940c8e898b437aa82fe575876f2946a2ad744a0c51501c7dfe6d7e5a31c58d2adc7a7dc4b87927594275ca235276accc9f628697a4c00b4e01", // certamen123
+  },
+];
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 // Escriba su código a partir de aquí
+
+app.get("/api", (req, res) => {
+  res.send("<h1>Hola Mundo!</h1>");
+});
 
 // Hasta aquí
 
 app.listen(PORT, (error) => {
-	if (error) {
-		console.error(`No se puede ocupar el puerto ${PORT} :(`)
-		return
-	}
+  if (error) {
+    console.error(`No se puede ocupar el puerto ${PORT} :(`);
+    return;
+  }
 
-	console.log(`Escuchando en el puerto ${PORT}`)
-})
+  console.log(`Escuchando en el puerto ${PORT}`);
+});
