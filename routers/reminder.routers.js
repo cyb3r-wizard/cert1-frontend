b23ade2a2d1ece88fbebbb2d5507e59f60b3ebeb
Mario H. Tornería Ramírez <mario.torneria@virginiogomez.cl>
2025-04-07 23:47:03 +0000

    eliminacion de reminders

--- routers/reminder.routers.js
+++ routers/reminder.routers.js
@@ -4,32 +4,68 @@ import crypto from "node:crypto";
 const reminderRouter = Router();
 
 reminderRouter.get("/", (req, res) => {
-  res.status(200).json({
-    message: "AUTORIZADO: AQUI SE MOSTRARÁN LAS TAREAS",
-    reminders: reminders,
-  });
+  res.status(200).send(reminders);
 });
 
 reminderRouter.post("/", (req, res) => {
-  const { content, important } = req.body;
-  const reminder = {
-    id: crypto.randomUUID(),
-    content: content,
-    createdAt: Date.now(),
-    important: important,
-  };
-  reminders.push(reminder);
-  res.status(200).json({ reminders: reminders });
+  try {
+    const { content, important } = req.body;
+    const reminder = {
+      id: crypto.randomUUID(),
+      content: content,
+      createdAt: Date.now(),
+      important: important,
+    };
+    reminders.push(reminder);
+    res.status(201).json(reminder);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: `ERROR: ${error} datetime:${reminder.createdAt}` });
+  }
 });
 
 reminderRouter.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  res.json({ message: `Has ingresado el id: ${id} ` });
+  try {
+    const id = req.params;
+    const reminder = reminders.find((rem) => rem.id);
+
+    if (!reminder) {
+      return res.status(404);
+    }
+
+    const { important, content } = req.body;
+
+    console.log(`${important} + ${content}`);
+
+    if (content) {
+      console.log("hay content");
+      reminder.content = content;
+    }
+
+    if (typeof important === "Boolean") {
+      console.log("hay important");
+      reminder.important = important;
+    }
+
+    return res.json({ message: `Has ingresado el id: ${id} ` });
+  } catch (error) {
+    return res.status(400).json({ error: `ERROR ${error}` });
+  }
 });
 
 reminderRouter.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  res.json({ message: `Has borrado el id ${id}` });
+  try {
+    const { id } = req.params;
+    res.json({ message: `Has borrado el id ${id}` });
+    let index = reminders.pop(reminders.find((reminder) => reminder.id));
+    if (!index) {
+      return res.status(404);
+    }
+    res.status(204);
+  } catch (error) {
+    res.status(400).json({ error: `ERROR ${error}` });
+  }
 });
 
 export default reminderRouter;
